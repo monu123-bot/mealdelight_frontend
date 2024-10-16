fcc75bea6b4d1912f988757dbce071e1ab1f6cfc
Monu Dixit <monudixit0007@gmail.com>
2024-10-16 23:41:15 +0530

    reg form upd

--- src/components/UserCreationForm.js
+++ src/components/UserCreationForm.js
@@ -2,96 +2,331 @@ import React, { useState } from 'react';
 import '../style/UserCreationForm.css';
 import axios from 'axios';
 import { host } from '../script/variables';
+import ProgressBar from "@ramonak/react-progress-bar";
+
+import { fn } from 'moment/moment';
 
 function UserCreationForm() {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    role: 'customer',
-    password: '',
-    street: '',
-    city: '',
-    state: '',
-    address: '',
-    postalCode: '',
-    apartment: '',
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const [fName,setfname] = useState(null)
+  const [lName,setlName] = useState(null)
+  const [phone,setPhone] = useState(null)
+  const [email,setEmail] = useState(null)
+  const [password,setPassword] = useState(null)
+  const [street,setStreet] = useState(null)
+  const [apartment,setApartment] = useState(null)
+  const [city,setCity] = useState(null)
+  const [state,setState] = useState(null)
+  const [postalcode,setPostalCode] = useState(null)
+  const [address,setAddress] = useState(null)
+  const [name,setName] = useState(null)
+  const [index,setIndex] = useState(0)
+ const [postOffices,setPostOffices] = useState([])
+ const [selectedPostOffice, setSelectedPostOffice] = useState('');  // Selected post office
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`${host}/user/create`, formData);
-      alert(response.data.msg);
-    } catch (error) {
-      alert('Error creating user: ' + error.response?.data?.msg || error.message);
+ const handlePostOfficeChange = (e) => {
+  const selectedPostOfficeName = e.target.value;
+  setSelectedPostOffice(selectedPostOfficeName);
+
+  // Find the selected post office from the array
+  const selectedPostOffice = postOffices.find(postOffice => postOffice.Name === selectedPostOfficeName);
+
+  if (selectedPostOffice) {
+    setState(selectedPostOffice.State);
+    setCity(selectedPostOffice.Region);
+  }
+};
+  const handleInputChange = (name, value) => {
+    switch (name) {
+      case 'fName':
+        setfname(value);
+        break;
+      case 'lName':
+        setlName(value);
+        break;
+      case 'phone':
+        setPhone(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      case 'postalCode':
+        setPostalCode(value);
+        break;
+      case 'street':
+        setStreet(value);
+        break;
+      case 'apartment':
+        setApartment(value);
+        break;
+      case 'address':
+        setAddress(value);
+        break;
+      default:
+        break;
     }
   };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await axios.post(`${host}/user/create`, formData);
+  //     alert(response.data.msg);
+  //   } catch (error) {
+  //     alert('Error creating user: ' + error.response?.data?.msg || error.message);
+  //   }
+  // };
+  const handlePostalCodeChange =async (code)=>{
+    // console.log(code)
+    setPostalCode(code)
+    if (code.length === 6) { // Assuming postal code length is 6
+      console.log(code)
+      try {
+        const response = await axios.get(`https://api.postalpincode.in/pincode/${code}`);
+        if (response.data[0].Status === 'Success') {
+          console.log(response.data[0].PostOffice)
+          setPostOffices(response.data[0].PostOffice || []);
+        } else {
+          setPostOffices([]); // Clear post offices if not found
+        }
+      } catch (error) {
+        console.error('Error fetching post offices:', error);
+      }
+    } else {
+      setPostOffices([]); // Reset post offices if postal code is not valid
+    }
 
+  }
+  const columns = ['name','phone','email','password','postalCode','street','apartment','address']
+  const content = [
+    {
+      html: (
+        <>
+          <p className='reg-form-h'>Your Name</p><br/>
+          <input 
+            className='inp' 
+            type="text" 
+            value={fName} 
+            onChange={(e) => handleInputChange('fName', e.target.value)} 
+            placeholder='First Name' 
+          />
+          <br/>
+          <input 
+            className='inp' 
+            type="text" 
+            value={lName} 
+            onChange={(e) => handleInputChange('lName', e.target.value)} 
+            placeholder='Last Name' 
+          />
+        </>
+      )
+    },
+    {
+      html: (
+        <>
+          <p className='reg-form-h'>Hy {fName} <br/>What's your contact number</p><br/>
+          <input 
+            className='inp' 
+            type="text" 
+            value={phone} 
+            onChange={(e) => handleInputChange('phone', e.target.value)} 
+            placeholder='phone' 
+          />
+          <br/>
+        </>
+      )
+    },
+    {
+      html: (
+        <>
+          <p className='reg-form-h'>Hy {fName}! <br/> What's Your email</p><br/>
+          <input 
+            className='inp' 
+            type="email" 
+            value={email} 
+            onChange={(e) => handleInputChange('email', e.target.value)} 
+            placeholder='email' 
+          />
+          <br/>
+        </>
+      )
+    },
+    {
+      html: (
+        <>
+          <p className='reg-form-h'>Hy {fName}! <br/>Set your password</p><br/>
+          <input 
+            className='inp' 
+            type="password" 
+            value={password} 
+            onChange={(e) => handleInputChange('password', e.target.value)} 
+            placeholder='password' 
+          />
+          <br/>
+        </>
+      )
+    },
+    {
+      html: (
+        <>
+          <p className='reg-form-h'>{fName} <br/> Let's connect <br/> Your address ?</p><br/>
+         
+        
+          <input 
+            className='inp' 
+            type="text" 
+            value={apartment} 
+            onChange={(e) => handleInputChange('apartment', e.target.value)} 
+            placeholder='apartment /flat /room' 
+          />
+          <br/>
+           <input 
+            className='inp' 
+            type="text" 
+            value={street} 
+            onChange={(e) => handleInputChange('street', e.target.value)} 
+            placeholder='street' 
+          />
+          
+          <br/>
+          <input 
+            className='inp' 
+            type="text" 
+            value={postalcode} 
+            onChange={(e) => handlePostalCodeChange(e.target.value)} 
+            placeholder='postal code' 
+          />
+          <br/>
+          
+          {
+          postOffices && postOffices.length > 0 && (
+        <select className="inp" value={selectedPostOffice} onChange={handlePostOfficeChange}  style={{ marginTop: '7px', marginBottom: '7px', marginLeft: '2px', marginRight: '2px' }} >
+          <option value="">Select Post Office</option>
+          {postOffices.map((postOffice,index) => (
+            <option key={index} value={postOffice.Name}>
+              {postOffice.Name}
+            </option>
+          ))}
+        </select>
+      )}
+      <br/>
+      <input 
+            className='inp' 
+            type="text" 
+            value={city} 
+            
+            placeholder='City' 
+          />
+          <br/>
+           <input 
+            className='inp' 
+            type="text" 
+            value={state} 
+            
+            placeholder='State' 
+          />
+      <br/>
+          <input 
+            className='inp' 
+            type="text" 
+            value={address} 
+            onChange={(e) => handleInputChange('address', e.target.value)} 
+            placeholder='address' 
+          />
+          <br/>
+        </>
+      )
+    }
+  ];
+  const isNameValid = ()=>{
+    return false
+  }
+  const isEmailValid = ()=>{
+    return false
+  }
+  const isPhoneValid = ()=>{
+    return false
+  }
+  const isPasswordValid = ()=>{
+    return false
+  }
+  const handleNext = ()=>{
+    if(index==0){
+      if(isNameValid()){
+          alert('Enter name')
+          return
+      }
+    }
+    if(index==1){
+      if(isPhoneValid()){
+        alert("Enter Phone Number")
+        return
+      }
+    }
+    if(index==2){
+      if(isEmailValid()){
+        alert("Enter Email")
+        return
+      }
+    }
+    if(index==3){
+      if(isPasswordValid()){
+        alert("Enter Email")
+        return
+      }
+    }
+    
+      if(index+1<content.length){
+        setIndex(index+1)
+      }
+      
+  }
+  const handlePrev = ()=>{
+     if(index-1>=0){
+      setIndex(index-1)
+     }
+  }
   return (
-    <><div className='logo_div'>
-    <img className='logo_img' src='/logo_transparent.png' />
+    <>
+    <div className='regform-container'>
+
+    
+    <div className='logo_div'>
+    <img className='logo_img' src='/logo_transparent.svg' />
     
     </div>
-    <div className="user-creation-form">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>First Name:</label>
-          <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label>Last Name:</label>
-          <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} />
-        </div>
-        <div className="form-group">
-          <label>Email:</label>
-          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label>Phone:</label>
-          <input type="text" name="phone" value={formData.phone} onChange={handleChange} required />
-        </div>
         
-        <div className="form-group">
-          <label>Password:</label>
-          <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-        </div>
-        <h3>Delivery Address Details</h3>
-        <div className="form-group">
-          <label>Street:</label>
-          <input type="text" name="street" value={formData.street} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label>Apartment/Flat/Floor :</label>
-          <input type="text" name="apartment" value={formData.apartment} onChange={handleChange} />
-        </div>
-        <div className="form-group">
-          <label>City:</label>
-          <input type="text" name="city" value={formData.city} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label>State:</label>
-          <input type="text" name="state" value={formData.state} onChange={handleChange} required />
-        </div>
+    <div className='updateprofmaindiv'>
+      <div className='progressbar'><ProgressBar isLabelVisible={false}  height='6px' bgColor='green' completed={((index+1)/content.length)*100} /></div>
+      
+      
+      <div className='mainDiv comp'>
+        {content[index].html}
+      </div>
+      <br/>
+      <div className='compnb'>
         
-        <div className="form-group">
-          <label>Postal Code:</label>
-          <input type="text" name="postalCode" value={formData.postalCode} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label>Full Address</label>
-          <input type="text" name="address" value={formData.address} onChange={handleChange} required />
-        </div>
-        
-        <button type="submit" className="btn">Save</button>
-      </form>
-    </div></>
+   
+        {
+          (index===0 || index===content.length-1) ? ' ' : <div className='innerCont' onClick={()=>{handlePrev()}}>Prev</div>
+        }
+      
+      {
+        (index===content.length-1) ? '' :       <div className='innerCont' onClick={()=>{handleNext()}}>Save & Next</div>
+
+      }
+    
+  
+    
+      </div>
+    </div>
+
+      
+    </div>
+    
+    </>
   );
 }
 
