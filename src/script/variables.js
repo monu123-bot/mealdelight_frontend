368710bb3a5f3b704f940e6b61954be2f5a312db
Monu Dixit <monudixit0007@gmail.com>
2024-12-21 13:32:05 +0530

    map added

--- src/script/variables.js
+++ src/script/variables.js
@@ -1,4 +1,4 @@
-// const host = 'https://mealdelight-backend.onrender.com'
-const host  = 'http://localhost:3002'
+const host = 'https://mealdelight-backend.onrender.com'
+// const host  = 'http://localhost:3002'
 
 export {host};
